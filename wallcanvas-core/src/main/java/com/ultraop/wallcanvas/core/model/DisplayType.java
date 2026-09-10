5c85b5735cbb867e6840404266ac6dd523c40b7a
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-10 22:44:52 +0530

    Define WallCanvas display types as Painting and Map

--- wallcanvas-core/src/main/java/com/ultraop/wallcanvas/core/model/DisplayType.java
+++ wallcanvas-core/src/main/java/com/ultraop/wallcanvas/core/model/DisplayType.java
@@ -1,6 +1,12 @@
 package com.ultraop.wallcanvas.core.model;
 
+/**
+ * The two independent WallCanvas display systems.
+ */
 public enum DisplayType {
-    IMAGE,
+    /** A custom Minecraft Painting used to display a WallCanvas picture. */
+    PAINTING,
+
+    /** A normal Minecraft map-based display. */
     MAP
 }
