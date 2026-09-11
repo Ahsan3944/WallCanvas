7882090254b9f134bbfeccb18712a71fa048900b
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-11 15:41:35 +0530

    Fix Fabric map command tree syntax

--- wallcanvas-fabric/src/main/java/com/ultraop/wallcanvas/fabric/map/FabricMapCommands.java
+++ wallcanvas-fabric/src/main/java/com/ultraop/wallcanvas/fabric/map/FabricMapCommands.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.ultraop.wallcanvas.core.DisplayStore;
+import com.ultraop.wallcanvas.core.WallCanvasCore;
 import com.ultraop.wallcanvas.core.library.ImageLibrary;
 import com.ultraop.wallcanvas.core.map.MapSpec;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -30,26 +31,39 @@ public final class FabricMapCommands {
 
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            var map = Commands.literal("map")
-                    .then(Commands.literal("create")
-                            .then(Commands.argument("picture", StringArgumentType.word())
-                                    .executes(context -> create(context.getSource().getPlayerOrException(), StringArgumentType.getString(context, "picture")))
-                                    .then(Commands.argument("x", DoubleArgumentType.doubleArg())
-                                            .then(Commands.argument("y", DoubleArgumentType.doubleArg())
-                                                    .then(Commands.argument("z", DoubleArgumentType.doubleArg())
-                                                            .executes(context -> create(context.getSource().getPlayerOrException(), StringArgumentType.getString(context, "picture"),
-                                                                    DoubleArgumentType.getDouble(context, "x"), DoubleArgumentType.getDouble(context, "y"), DoubleArgumentType.getDouble(context, "z")))
-                                                            .then(Commands.argument("width", IntegerArgumentType.integer(1, 16))
-                                                                    .then(Commands.argument("height", IntegerArgumentType.integer(1, 16))
-                                                                            .executes(context -> create(context.getSource().getPlayerOrException(), StringArgumentType.getString(context, "picture"),
-                                                                                    DoubleArgumentType.getDouble(context, "x"), DoubleArgumentType.getDouble(context, "y"), DoubleArgumentType.getDouble(context, "z"),
-                                                                                    IntegerArgumentType.getInteger(context, "width"), IntegerArgumentType.getInteger(context, "height"))))))))
-                    .then(Commands.literal("give")
-                            .then(Commands.argument("picture", StringArgumentType.word())
-                                    .executes(context -> give(context.getSource().getPlayerOrException(), StringArgumentType.getString(context, "picture")))))
-                    .then(Commands.literal("remove")
-                            .then(Commands.argument("display", StringArgumentType.word())
-                                    .executes(context -> remove(context.getSource().getPlayerOrException(), StringArgumentType.getString(context, "display")))));
+            var createPicture = Commands.argument("picture", StringArgumentType.word())
+                    .executes(context -> create(context.getSource().getPlayerOrException(), StringArgumentType.getString(context, "picture")));
+
+            var height = Commands.argument("height", IntegerArgumentType.integer(1, 16))
+                    .executes(context -> create(context.getSource().getPlayerOrException(),
+                            StringArgumentType.getString(context, "picture"),
+                            DoubleArgumentType.getDouble(context, "x"),
+                            DoubleArgumentType.getDouble(context, "y"),
+                            DoubleArgumentType.getDouble(context, "z"),
+                            IntegerArgumentType.getInteger(context, "width"),
+                            IntegerArgumentType.getInteger(context, "height")));
+            var width = Commands.argument("width", IntegerArgumentType.integer(1, 16)).then(height);
+            var z = Commands.argument("z", DoubleArgumentType.doubleArg())
+                    .executes(context -> create(context.getSource().getPlayerOrException(),
+                            StringArgumentType.getString(context, "picture"),
+                            DoubleArgumentType.getDouble(context, "x"),
+                            DoubleArgumentType.getDouble(context, "y"),
+                            DoubleArgumentType.getDouble(context, "z")))
+                    .then(width);
+            var y = Commands.argument("y", DoubleArgumentType.doubleArg()).then(z);
+            var x = Commands.argument("x", DoubleArgumentType.doubleArg()).then(y);
+            createPicture.then(x);
+            var create = Commands.literal("create").then(createPicture);
+
+            var givePicture = Commands.argument("picture", StringArgumentType.word())
+                    .executes(context -> give(context.getSource().getPlayerOrException(), StringArgumentType.getString(context, "picture")));
+            var give = Commands.literal("give").then(givePicture);
+
+            var display = Commands.argument("display", StringArgumentType.word())
+                    .executes(context -> remove(context.getSource().getPlayerOrException(), StringArgumentType.getString(context, "display")));
+            var remove = Commands.literal("remove").then(display);
+
+            var map = Commands.literal("map").then(create).then(give).then(remove);
             dispatcher.register(Commands.literal("wallcanvas").then(map));
         });
     }
@@ -84,8 +98,7 @@ public final class FabricMapCommands {
                 player.sendSystemMessage(Component.literal("Picture not found."));
                 return 0;
             }
-            ItemStack map = new FabricMapDisplayManager(picturesDirectory, displayStore)
-                    .createMapItem(player.serverLevel(), asset);
+            ItemStack map = new FabricMapDisplayManager(picturesDirectory, displayStore).createMapItem(player.serverLevel(), asset);
             if (!player.getInventory().add(map)) player.drop(map, false);
             player.sendSystemMessage(Component.literal("Gave WallCanvas Map for '" + asset + "'."));
             return 1;
@@ -115,7 +128,7 @@ public final class FabricMapCommands {
             Path path = picturesDirectory.resolve(name).normalize();
             return path.getParent().equals(picturesDirectory.toAbsolutePath().normalize())
                     && Files.isRegularFile(path)
-                    && com.ultraop.wallcanvas.core.WallCanvasCore.isSupportedImageExtension(ImageLibrary.extension(path));
+                    && WallCanvasCore.isSupportedImageExtension(ImageLibrary.extension(path));
         } catch (Exception ignored) {
             return false;
         }
